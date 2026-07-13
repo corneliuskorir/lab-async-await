683f1737e2f1ab84787f828c3827f4c013ea71e7
Cornelius Korir <cornelius.korir@student.moringaschool.com>
2026-07-13 17:12:56 +0300

    tests passed

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your code here!
+
+function displayPosts(posts) {
+  const postList = document.querySelector("#post-list");
+
+  posts.forEach((post) => {
+    const listItem = document.createElement("li");
+
+    const title = document.createElement("h1");
+    title.textContent = post.title;
+
+    const body = document.createElement("p");
+    body.textContent = post.body;
+
+    listItem.append(title, body);
+
+    postList.appendChild(listItem);
+  });
+}
+
+async function fetchPosts() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+    const posts = await response.json();
+
+    displayPosts(posts);
+  } catch (error) {
+    console.log("Error retreiving posts: ", error);
+  }
+}
+
+fetchPosts();
